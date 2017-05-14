c21fcbc9067b12355e7959711c290e7f195abe2d
Falkirks <me@falkirks.com>
2017-05-14 07:49:29 -0700

    Version 3.3.4

--- src/falkirks/simplewarp/Version.php
+++ src/falkirks/simplewarp/Version.php
@@ -6,7 +6,7 @@ use pocketmine\command\CommandSender;
 
 class Version {
     const ENABLE_VERBOSE = false;
-    const VERSION_STRING = "v3.3.3";
+    const VERSION_STRING = "v3.3.4";
     private static $quotes = [
         "\"Still round the corner there may wait\nA new road or a secret gate\"\n- J.R.R. Tolkien",
         "\"But here, upon this bank and shoal of time,\n We’d jump the life to come.\"\n - William Shakespeare"
