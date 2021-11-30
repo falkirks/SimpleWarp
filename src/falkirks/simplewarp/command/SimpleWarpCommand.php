ffabfb906f717a73d35e9e0d6ba65da4ae064da5
DataLion <datalionmcpe@gmail.com>
2021-11-30 14:16:10 +0100

    Fixed getOwningPlugin method

--- src/falkirks/simplewarp/command/SimpleWarpCommand.php
+++ src/falkirks/simplewarp/command/SimpleWarpCommand.php
@@ -5,7 +5,6 @@ namespace falkirks\simplewarp\command;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginOwned;
-use pocketmine\plugin\Plugin;
 
 abstract class SimpleWarpCommand extends Command implements PluginOwned {
 
@@ -17,8 +16,8 @@ abstract class SimpleWarpCommand extends Command implements PluginOwned {
      * @return mixed
      */
     public function execute(CommandSender $sender, string $commandLabel, array $args) {
-        if($this->getPlugin()->isDisabled()){
-            $sender->sendMessage($this->getPlugin()->getApi()->executeTranslationItem("plugin-disabled"));
+        if($this->getOwningPlugin()->isDisabled()){
+            $sender->sendMessage($this->getOwningPlugin()->getApi()->executeTranslationItem("plugin-disabled"));
             return false;
         }
         return true;
